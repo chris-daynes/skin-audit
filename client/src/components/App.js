5d08e2b5243fa8ffb89f9f93ebcb72a87357e2a5
chris-daynes <chrisdaynes@gmail.com>
2017-08-20 07:45:29 +1200

    deleted all the files out of src folder and started new. Got react router working for the App

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h1>Header</h1>;
+const Landing = () => <h1>Landing</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+const NewPatient = () => <h1>NewPatient</h1>;
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className='container'>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route path='/new/patient' component={NewPatient} />
+          </div>
+        </BrowserRouter>
+      </div>
+    ); 
+  };
+}
+
+export default App;
